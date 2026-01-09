be2844fb31c806915dc98a34e3d82b1b7537cea5
vikramdev772 <vikramdev772@gmail.com>
2026-01-09 15:02:26 +0530

    fix class name of Main.java

--- myapp/src/main/java/com/example/myapp/Main.java
+++ myapp/src/main/java/com/example/myapp/Main.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public Main{
+public class Main{
     @GetMapping("/fruits")
     HashMap<Object,Object>  m(){
     HashMap<Object,Object> res=new HashMap<>();
